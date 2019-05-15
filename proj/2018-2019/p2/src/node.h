ac3b9a765815b5c07322bea47a246d0bcf9e6cac
Keyaku <antonio.luis.sarmento@gmail.com>
2019-05-15 15:48:24 +0100

    Minor fix

--- proj/2018-2019/p2/src/node.h
+++ proj/2018-2019/p2/src/node.h
@@ -17,12 +17,12 @@ typedef struct list {
 
 
 /* Protótipos */
-void list_add_node(List *l, char *name, char *email, char *phone));
+void list_add_node(List *l, char *name, char *email, char *phone);
 void list_print(List *l);
 void list_print_node(List *l, char *name);
 void list_remove_node(List *l, char *name);
 void list_change_email(List *l, char *name, char *email);
-void list_count_occurrences(List *l, char *domain);
+int list_count_occurrences(List *l, char *domain);
 void list_destroy(List *l);
 
 
