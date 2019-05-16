10738ca1c7f27e89cb8b5d056d6862fc16c80d36
Keyaku <antonio.luis.sarmento@gmail.com>
2019-05-16 19:23:16 +0100

    Fixed NULL pointer to start of AllNodes[idx]

--- proj/2018-2019/p2/src/hash.c
+++ proj/2018-2019/p2/src/hash.c
@@ -28,7 +28,7 @@ void hashtable_add_node(Node *n) {
 		AllNodes[idx] = calloc(1, sizeof(HashNode));
 		hn = AllNodes[idx];
 	} else {
-		for (hn = AllNodes[idx]; hn->next != NULL && strcmp(hn->n->name, n->name) != 0; hn = hn->next);
+		for (hn = AllNodes[idx]; hn->next != NULL; hn = hn->next);
 		hn->next = calloc(1, sizeof(HashNode));
 		hn = hn->next;
 	}
@@ -39,9 +39,11 @@ void hashtable_add_node(Node *n) {
 void hashtable_remove_node(Node *n) {
 	HashNode *hn, *next;
 	int idx = hash(n->name);
+
 	for (hn = AllNodes[idx], next = hn; next->n != n; hn = next, next = next->next);
+	if (hn == next) AllNodes[idx] = hn->next;
 	hn->next = next->next;
-	if (AllNodes[idx] == hn) AllNodes[idx] = NULL;
+
 	free(next);
 }
 
